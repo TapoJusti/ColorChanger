3de9818078d30b90d93341a1502716cd59bf3e4d
tapojusti <mastechinas@gmail.com>
2018-03-27 10:01:41 +0300

    dsdf comit

--- app/src/main/java/com/tapojusti/www/colorchanger/MainActivity.java
+++ app/src/main/java/com/tapojusti/www/colorchanger/MainActivity.java
@@ -1,13 +1,38 @@
 package com.tapojusti.www.colorchanger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button btnRed;
+    private Button btnGreen;
+    private Button btnBlue;
+    private Button btnBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        btnBlue = (Button) findViewById(R.id.btnBlue);
+        btnRed = (Button) findViewById(R.id.btnRed);
+        btnGreen = (Button) findViewById(R.id.btnGreen);
+        btnBackground = (Button) findViewById(R.id.btnBackground);
+
+        btnBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+//                textView.setBackgrounndColor(getResources().getColor(R.color.blue));
+            }
+
+        }
+
+
+        );
     }
 }
